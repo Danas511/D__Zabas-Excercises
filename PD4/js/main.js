14a6858c9379144fa7c415c68d1545618287c255
Danas <danas.zabas@stud.radvilu.lt>
2023-10-19 13:08:36 +0300

    PD4 commited

---
+++ PD4/js/main.js
@@ -0,0 +1,7 @@
+let petname = "name"
+let petage = 8438
+console.log(petname+ " " + petage)
+petname = "newname"
+petage = 5
+const address = "address"
+console.log(petname+ " " + petage + " " + address) 
